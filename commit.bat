5237455072ea0496737438af54ab4e05277033a0
fabynardon <39743117+fabynardon@users.noreply.github.com>
2018-09-09 00:15:43 -0300

    %mensaje%

--- commit.bat
+++ commit.bat
@@ -1,4 +1,4 @@
 @echo OFF
 set SVN_EDITOR=TYPE
 set /P mensaje=Mensaje del Commit:
-svn commit  -m %"%mensaje%"% --username fabian_nardone@hotmail.com
+svn commit  -m "%%mensaje%%" --username fabian_nardone@hotmail.com
